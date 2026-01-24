d8fb39eced12aa1dcf409ca977ac101ff7b0d0be
Giacomo Voltolina <giacomovoltolina@MacBookPro.Home>
2026-01-24 17:00:25 +0100

    Remove static /it route

--- src/app/it/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ItStaticPage() {
-  return (
-    <main style={{ padding: 40, fontFamily: "system-ui" }}>
-      <h1>VYC - IT static route OK</h1>
-      <p>Se vedi questa pagina, Vercel sta deployando correttamente.</p>
-    </main>
-  );
-}
